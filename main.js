a0a8c0da71dfd87d384377a447770ff42879b1d9
Lakshya-Agiwal <85127186+Lakshya-Agiwal@users.noreply.github.com>
2022-05-13 15:40:19 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,44 @@
+var nose_x = 0;
+var nose_y = 0;
+var img = "";
+
+function preLoad() {
+
+   // img = loadImage("https://i.postimg.cc/7ZBcjDqp/clownnose.png");
+}
+
+function setup() {
+    canvas = createCanvas(300, 300);
+    canvas.position(623.569, 100);
+    video = createCapture(VIDEO);
+    video.size(300, 300);
+    video.hide();
+    var posenet = ml5.poseNet(video, modelLoaded);
+    posenet.on("pose", got_poses);
+}
+
+function modelLoaded() {
+    console.log(ml5.version);
+
+}
+
+function draw() {
+    image(video, 0, 0, 300, 300);
+    fill("red");
+    circle(nose_x, nose_y, 40);
+   // image(img,nose_x,nose_y,40,40);
+}
+
+function got_poses(result) {
+    if (result.length != 0) {
+        console.log(result);
+        nose_x = result[0].pose.nose.x;
+        nose_y = result[0].pose.nose.y;
+        console.log(nose_x);
+        console.log(nose_y);
+    }
+}
+
+function shot() {
+    save("Filter.jpg");
+}
